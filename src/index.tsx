bdaa2ed376ec23d6c7d0d9b595d4238d5123b9f4
Nikos Dim <ndimitrakopoulos@maseurope.com>
2022-09-30 15:02:45 +0300

    fix eslint issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const container = document.getElementById("app");
+const container = document.getElementById('app');
 
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
